f7af1e198839444f9d3ecc3fe452e0e5ceef80e9
Fernando Morales <fezequielmorales@gmail.com>
2019-12-02 10:56:11 -0600

    Al parecer no habia commiteado eso

--- interfaz.h
+++ interfaz.h
@@ -7,10 +7,6 @@ class Interfaz{
     private:
         
         Grafo_MA grafo;
-        int* vectorDePesos;
-        vertice* vectorVerticeAnterior;
-        int** matrizDePesos;
-        vertice** matrizDeVerticesIntermedios;
         Algoritmos_grafo_MA algoritmos;
 
     public:
